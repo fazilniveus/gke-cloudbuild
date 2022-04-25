6c1f913436a81fbdbddacd7a7468370d434f801f
fazilniveus <73817323+fazilniveus@users.noreply.github.com>
2022-04-25 14:16:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 var port = process.env.PORT ? Number.parseInt(process.env.PORT) : 3000;
 
 app.get("/", function(req, res) {
-  res.send("Hello World 🌎");
+  res.send("Hey! Hello World 🌎");
 });
 
 app.listen(3000, function() {
